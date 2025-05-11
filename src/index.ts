213805e0fab06b5153e7ca89343102240c940c8f
Diana_Bobrova <dziana_babrova@epam.com>
2025-05-11 22:21:16 +0300

    feat: store port in .env file

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ const server = createServer(
   }
 );
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
