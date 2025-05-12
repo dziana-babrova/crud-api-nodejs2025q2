8893e747223637440d6c3ff71ce98c7f18f298a6
Diana_Bobrova <dziana_babrova@epam.com>
2025-05-13 01:32:50 +0300

    fix: fix headers error

--- src/helpers/sendResponse.ts
+++ src/helpers/sendResponse.ts
@@ -5,6 +5,7 @@ export const sendResponse = async <T>(
   statusCode: number,
   data: T
 ) => {
+  if (response.writableEnded) return;
   response.writeHead(statusCode, { 'Content-Type': 'application/json' });
   response.end(JSON.stringify(data));
 };
